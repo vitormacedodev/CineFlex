8937682e5bfc117ca28a87c6b611a11190ccce2e
Vitor Macedo <vitorjvbm@gmail.com>
2022-03-23 01:13:02 -0300

    defined routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Catalog from "./Pages/Catalog/Index";
+import Sessions from "./Pages/Sessions/Index";
+import Seats from "./Pages/Seats/Index";
+import Success from "./Pages/Success/Index";
 
 export default function App(){
     return(
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route />
+                    <Route path="/" element={<Catalog />} />
+                    <Route path="/sessions/:movieID" element={<Sessions />} />
+                    <Route path="/sessions/:sessionID" element={<Seats />} />
+                    <Route path="/success" element={<Success />} />
                 </Routes>
             </BrowserRouter>
         </>
