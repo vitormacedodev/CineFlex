6ceebb0ecfbf4c02198048d06da9a13ce818209f
Vitor Macedo <vitorjvbm@gmail.com>
2022-03-26 20:52:17 -0300

    feature: footer

--- src/Components/Poster/Index.jsx
+++ src/Components/Poster/Index.jsx
@@ -1,10 +1,16 @@
 import styled from "styled-components"
 
-export default function Poster({title, posterURL}){
+export default function Poster({title, posterURL, weekday, showtime}){
     return(
         <>
             <PosterBox>
-                <h1></h1>
+                <PosterDiv>
+                    <img src={posterURL} alt={title} />
+                </PosterDiv>
+                <SessionInfo>
+                    <h3>{title}</h3>
+                    {(weekday&&showtime) && <h3>{weekday} - {showtime}</h3>}
+                </SessionInfo>
             </PosterBox>
         </>
     )
@@ -12,11 +18,44 @@ export default function Poster({title, posterURL}){
 
 const PosterBox = styled.footer`
     height: 117px;
-    
+    width: 100%;
+    padding: 0 10px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    gap: 14px;
     position: fixed;
     bottom: 0;
+    background-color: #9EADBA;;
+    border: 1px solid #9EADBA;
 
-    background-color: #DFE6ED;
-;
+`
 
+const PosterDiv = styled.div`
+    width: 64px;
+    height: 89px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #FFFFFF;
+
+    img{
+        width: 48px;
+        height: 72px;
+    }
+`
+
+const SessionInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 7px;
+    
+    h3{
+        font-family: 'Roboto', sans-serif;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 26px;
+        color: #293845;
+    }   
 `
