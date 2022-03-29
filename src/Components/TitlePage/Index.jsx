753e7ae4f542bf237e7b125f7eeebe35b019d6ea
Vitor Macedo <vitorjvbm@gmail.com>
2022-03-28 23:41:02 -0300

    fixes

--- src/Components/TitlePage/Index.jsx
+++ src/Components/TitlePage/Index.jsx
@@ -3,7 +3,7 @@ import { TitleContainer } from "./Style";
 export default function TitlePage({fontWeight, color, title, top, bot}){
     return(
         <>
-            <TitleContainer fontWeight={fontWeight} color={'red'} top={top} bot={bot}>
+            <TitleContainer fontWeight={fontWeight} color={color} top={top} bot={bot}>
                 <h2>{title}</h2>
             </TitleContainer>
         </>
