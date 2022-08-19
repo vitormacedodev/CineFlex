91afabeff06cf38f9f628dfa486ee70290056b97
José Vitor <86400522+Vitor7rs@users.noreply.github.com>
2022-08-19 03:36:34 -0300

    Update Style.jsx

--- src/Components/TitlePage/Style.jsx
+++ src/Components/TitlePage/Style.jsx
@@ -1,21 +1,17 @@
 import styled from "styled-components"
 
 export const TitleContainer = styled.header`
-    
     width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
     margin-top: ${props => props.top}px;
     margin-bottom: ${props => props.bot}px;
-
     h2{
-
         font-family: 'Roboto', sans-serif;
         font-style: normal;
         font-weight:${props => props.fontWeight};
         font-size: 24px;
-        
         color: ${props => props.color};
     }
 `
